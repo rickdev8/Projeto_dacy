fabf9e74ec8bc69ff1ec7189d462cb704e993f51
rickdev8 <rique88858@gmail.com>
2025-09-18 06:26:39 -0300

    finals

--- src/app/services/FindSale.ts
+++ src/app/services/FindSale.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const FindSale = async (id: string) => {
-  const res = await axios.get(`http://localhost:3000/homepage/FindSale/${id}`);
+  const res = await axios.get(`https://projeto-back-dacy.onrender.com/homepage/FindSale/${id}`);
   return res.data;
 };
--- src/app/services/LoginUser.ts
+++ src/app/services/LoginUser.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-export const Login = async (data: {
-  email: string;
-  password: string;
-}) => {
+export const Login = async (data: { email: string; password: string }) => {
   const response = await axios.post(
-    `http://localhost:3000/homepage/login`,
+    `https://projeto-back-dacy.onrender.com/homepage/login`,
     data
   );
   return response;
