07f7a96691d1d590406e3833047fde6562b18ff5
ridckdev5 <habade60@gmail.com>
2025-09-19 21:33:07 -0300

    awfa

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -42,7 +42,7 @@ export default function WarehousesPage() {
         setSales([...response.data.sales]);
         setLoading(false);
       }
-    }, 1200);
+    }, 1400);
   };
 
   useEffect(() => {
